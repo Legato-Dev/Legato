f9a49190bcab430e7f6756cffb80227723c93c78
Asterisk <kuro5858tube@gmail.com>
2017-09-19 20:01:05 +0900

    #11 Modified Legato.Sample
    CurrentTrack Duration Async Fix.

--- Legato.Sample/Form1.cs
+++ Legato.Sample/Form1.cs
@@ -3,14 +3,44 @@ using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 
+using static System.Console;
+using System.Threading.Tasks;
+
 namespace Legato.Sample
 {
 	public partial class Form1 : Form
 	{
-		public Form1()
+        #region Field
+
+        private System.Timers.Timer _timer;
+        private uint TimerCounter;
+        private uint MinuteCounter;
+        private uint SecondCounter;
+        private bool OnlyTimerStartFlg;
+
+        #endregion
+
+        #region Constants
+
+        const uint msConvertSec = 1000;
+        const uint betweenMin = 59;
+
+        #endregion
+
+        public Form1()
 		{
 			InitializeComponent();
+            Initialize();
 		}
+
+        public void Initialize()
+        {
+            _timer = new System.Timers.Timer();
+            TimerCounter = 0;
+            MinuteCounter = 0;
+            SecondCounter = 0;
+            OnlyTimerStartFlg = false;
+        }
 
 		private Legato _Legato { get; set; }
 
@@ -24,19 +54,26 @@ namespace Legato.Sample
 			_Legato = null;
 		}
 
-		private void buttonFetch_Click(object sender, EventArgs e)
-		{
-			Console.WriteLine($"IsRunning:{_Legato.IsRunning}");
+        private void DisSigneture()
+        {
+            TimerCounter = 0;
+            MinuteCounter = 0;
+            SecondCounter = 0;
+        }
 
-			if (_Legato?.IsRunning ?? false)
+        private void buttonFetch_Click(object sender, EventArgs e)
+		{
+			WriteLine($"IsRunning:{_Legato.IsRunning}");
+
+            if (_Legato?.IsRunning ?? false)
 			{
-				Console.WriteLine($"State:{_Legato.State}");
-				Console.WriteLine($"IsShuffle:{_Legato.IsShuffle}");
-				Console.WriteLine($"Volume:{_Legato.Volume}");
-				Console.WriteLine($"Position:{_Legato.Position} - {_Legato.Duration}");
+				WriteLine($"State:{_Legato.State}");
+				WriteLine($"IsShuffle:{_Legato.IsShuffle}");
+				WriteLine($"Volume:{_Legato.Volume}");
+				WriteLine($"Position:{_Legato.Position} - {_Legato.Duration}");
 
 				var albumArt = _Legato.AlbumArt;
-				Console.WriteLine($"AlbumArt: {albumArt != null} {albumArt.Length}");
+				WriteLine($"AlbumArt: {albumArt != null} {albumArt.Length}");
 
 				if ((albumArt?.Length ?? 0) != 0)
 				{
@@ -47,16 +84,60 @@ namespace Legato.Sample
 					}
 				}
 			}
-		}
+        }
 
 		private void buttonPlayPause_Click(object sender, EventArgs e)
 		{
 			if (_Legato?.IsRunning ?? false)
 			{
 				if (_Legato.State == Interop.Aimp.Enum.PlayerState.Playing)
-					_Legato.Pause();
-				else
-					_Legato.Play();
+                {
+                    _Legato.Pause();
+                    _timer.Stop();
+                }
+                else
+                {
+                    Task.Run(async () =>
+                    {
+                        _Legato.Play();
+
+                        var time      = _Legato.Position / msConvertSec;
+                        var customize = _Legato.Position - (uint)time * msConvertSec;
+
+                        await Task.Delay(new TimeSpan(customize));
+                        TimerCounter = (uint)time;
+
+                        CurrentPos.Invoke((Action)(() =>
+                        {
+                            CurrentPos.Text = $"Duration = {MinuteCounter} : {SecondCounter}";
+                        }));
+
+                        if (OnlyTimerStartFlg == false)
+                        {
+                            _timer.Elapsed += (s, v) =>
+                            {
+                                ++TimerCounter;
+                                CurrentPos.Invoke((Action)(() =>
+                                {
+                                    if (SecondCounter == betweenMin)
+                                    {
+                                        ++MinuteCounter;
+                                        SecondCounter = 0;
+                                    }
+                                    else
+                                        ++SecondCounter;
+
+                                    CurrentPos.Text = $"Duration = {MinuteCounter} : {string.Format("{0:D2}", SecondCounter)}";
+                                }));
+                            };
+
+                            _timer.Interval = msConvertSec;
+                            OnlyTimerStartFlg = true;
+                        }
+
+                        _timer.Start();
+                    }); 
+                }
 			}
 		}
 
@@ -65,6 +146,7 @@ namespace Legato.Sample
 			if (_Legato?.IsRunning ?? false)
 			{
 				_Legato.Next();
+                DisSigneture();
 			}
 		}
 
@@ -73,7 +155,8 @@ namespace Legato.Sample
 			if (_Legato?.IsRunning ?? false)
 			{
 				_Legato.Prev();
-			}
+                DisSigneture();
+            }
 		}
 	}
 }
