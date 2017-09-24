d59ee4582ff819b6954d33e02a5c676f3e29b2ca
Marihachi <marihachi0620@gmail.com>
2017-09-25 04:46:05 +0900

    add album art extraction project

---
+++ Legato.AlbumArtExtraction/FlacAlbumArtExtractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+
+namespace Legato.AlbumArtExtraction
+{
+	/// <summary>
+	/// FLAC形式のファイルからアルバムアートを抽出する機能を表します
+	/// </summary>
+	public class FlacAlbumArtExtractor : IAlbumArtExtractor
+	{
+		public Image Extract(string filePath)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Legato.AlbumArtExtraction/IAlbumArtExtractor.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace Legato.AlbumArtExtraction
+{
+	/// <summary>
+	/// アルバムアートを抽出するために必要となるメンバを公開します
+	/// </summary>
+	public interface IAlbumArtExtractor
+	{
+		Image Extract(string filePath);
+	}
+}
