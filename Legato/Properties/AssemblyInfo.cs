5425b68578cf95cca5b599b2c9d887256d197481
marihachi <marihachi0620@gmail.com>
2017-12-29 05:11:55 +0900

    update

--- Legato/Properties/AssemblyInfo.cs
+++ Legato/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Legato")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 Legato-Dev")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
